3252ee09dd69fe2255e9d55c8f4dcae16388baf9
ChristopherYammine <chrisyammine98@gmail.com>
2022-07-11 11:50:49 +0300

    back end : added check if exists condition on the add attendee api

--- Closer-backend/app/Http/Controllers/AttendeeController.php
+++ Closer-backend/app/Http/Controllers/AttendeeController.php
@@ -21,20 +21,34 @@ class AttendeeController extends Controller
     {
         $event = new Event();
         $capacity = Event::where('id', '=', $event_id)->get("capacity")[0]["capacity"];
-        $count = $this->model->where('event_id', '=', $event_id)->count();
-        if ($capacity > $count) {
-            $data['event_id'] = $event_id;
-            $data['user_id'] = Auth::id();
-            $attendee = $this->model->create($data);
-            return response()->json([
-                "status" => "success",
-                "data" => compact('attendee')
-            ], 200);
-        } else {
+        $userExist = $this->model
+        ->where('event_id', '=', $event_id)
+        ->where('user_id', '=', Auth::id())->count();
+        $count = $this->model
+        ->where('event_id', '=', $event_id)->count();
+
+        if ($userExist>=1){
             return response()->json([
                 "status" => "failed",
-                "data" => "exceeded event capacity"
+                "data" => "user exists"
             ], 200);
+        } else {
+            if ($capacity > $count) {
+                $data['event_id'] = $event_id;
+                $data['user_id'] = Auth::id();
+                $attendee = $this->model->create($data);
+                return response()->json([
+                    "status" => "success",
+                    "data" => compact('attendee')
+                ], 200);
+            } else {
+                return response()->json([
+                    "status" => "failed",
+                    "data" => "exceeded event capacity"
+                ], 200);
+            }
         }
+
+       
     }
 }
