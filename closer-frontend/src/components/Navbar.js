6a67ddfd1a1f2376462a12cd5fa0036bb37ded2e
ChristopherYammine <chrisyammine98@gmail.com>
2022-07-16 16:00:34 +0300

    frontend : started a conditional navbar

---
+++ closer-frontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className='header'>
+
+                <Logo />
+                <div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
