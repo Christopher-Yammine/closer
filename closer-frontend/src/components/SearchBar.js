9d319b216f0cb1e157081b89c46f04ae23f662bd
ChristopherYammine <chrisyammine98@gmail.com>
2022-07-16 23:44:15 +0300

    frontend : added styling to the searchbar

--- closer-frontend/src/components/SearchBar.js
+++ closer-frontend/src/components/SearchBar.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
+    return (
+        <div className='searchbar-container'>
+
+            <div className='searchbar-content'>
+                <div>
+                    Find your universe...
+                </div>
+                <div className='searchbar'>
+
+                    <div>
+                        <input type="text" placeholder='Date | Location' />
+                    </div>
+                    <div className='magnifier'>
+                        <img src={require('../assets/magnifier.png')} width={56} />
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default SearchBar
