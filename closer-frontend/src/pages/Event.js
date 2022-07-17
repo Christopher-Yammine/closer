17182678e2b5537b59d13deeba141fbb15497208
ChristopherYammine <chrisyammine98@gmail.com>
2022-07-18 02:10:28 +0300

    frontend : making the event clickable to redirect to its info page

---
+++ closer-frontend/src/pages/Event.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Event = () => {
+    return (
+        <>Navbar</>
+    )
+}
+
+export default Event
--- closer-frontend/src/pages/Events.js
+++ closer-frontend/src/pages/Events.js
@@ -2,8 +2,13 @@ import React from 'react'
 import EventCard from '../components/EventCard'
 import Navbar from '../components/Navbar'
 import SearchBar from '../components/SearchBar'
+import { useNavigate } from 'react-router-dom'
 
 const Events = () => {
+    const navigate = useNavigate();
+    function redirect() {
+        navigate("/event");
+    }
     return (
         <div className='events-section'>
             <div>
@@ -11,7 +16,7 @@ const Events = () => {
                 <SearchBar />
             </div>
             <div className='events-display'>
-                <div className='event-container'>
+                <div className='event-container' onClick={redirect}>
                     <EventCard title="event 1" />
                 </div>
                 <div className='event-container'>
