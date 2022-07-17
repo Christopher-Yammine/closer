683c373aac30d3d03d4a071d814b9e2878e9d368
ChristopherYammine <chrisyammine98@gmail.com>
2022-07-18 01:51:29 +0300

    frontend : added responsiveness to the events page

--- closer-frontend/src/pages/Events.js
+++ closer-frontend/src/pages/Events.js
@@ -1,12 +1,70 @@
 import React from 'react'
+import EventCard from '../components/EventCard'
 import Navbar from '../components/Navbar'
 import SearchBar from '../components/SearchBar'
 
 const Events = () => {
     return (
         <div className='events-section'>
-            <Navbar />
-            <SearchBar />
+            <div>
+                <Navbar />
+                <SearchBar />
+            </div>
+            <div className='events-display'>
+                <div className='event-container'>
+                    <EventCard title="event 1" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 2" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 3" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 4" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 1" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 2" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 3" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 4" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 1" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 2" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 3" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 4" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 1" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 2" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 3" />
+                </div>
+                <div className='event-container'>
+                    <EventCard title="event 4" />
+                </div>
+
+
+
+            </div>
+
+
         </div>
     )
 }
