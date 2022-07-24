ccb99bc0de202de5e401b374b08099cbb16f451d
ChristopherYammine <chrisyammine98@gmail.com>
2022-07-24 20:59:41 +0300

    frontend : added an upload profile picture section

--- closer-frontend/src/components/SignupModal.js
+++ closer-frontend/src/components/SignupModal.js
@@ -16,13 +16,29 @@ const SignupModal = ({ signupIsOpen, handleSignupOpen }) => {
                             Set up your <span className='purple'>Closer</span> account
                         </div>
                         <div className='signup-input'>
+                            <input type="text" placeholder='Enter your first name'></input>
+                            <input type="text" placeholder='Enter your last name'></input>
                             <input type="text" placeholder='Enter your username'></input>
                             <input type="password" placeholder='Enter your password'></input>
+                            <select>
+                                <option value="0">Female</option>
+                                <option value="1">Male</option>
+                                <option value="2">Other</option>
+                            </select>
                         </div>
 
                     </div>
                     <div className='right-signup'>
-                        <button type="button" className='signup-btn'></button>
+                        <div className='upload-profile'>
+                            <input type="file" name="photo" id="fileId2"
+                                onChange={(e) => { }} />
+                            <div className="upload">
+                                <div className='cover-photo-host'>
+                                    <img src={''} alt='' />
+                                </div>
+                            </div>
+                            <label className='cover-photo-desc' htmlFor="fileId2">click here to upload your event card photo </label>
+                        </div>
                     </div>
                 </div>
             </Box>
