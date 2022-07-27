0cc0d6b1419d013c5da6a1bd8707943e5373caaf
ChristopherYammine <chrisyammine98@gmail.com>
2022-07-27 15:49:52 +0300

    frontend : added a title to each page and initiated a user stats container

---
+++ closer-frontend/src/components/PageHeading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageHeading = ({ text }) => {
+    return (
+        <div className='page-heading'>
+            <h1>{text}</h1>
+        </div>
+    )
+}
+
+export default PageHeading
--- closer-frontend/src/pages/AdminPanel.js
+++ closer-frontend/src/pages/AdminPanel.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import PageNotFound from './PageNotFound'
 import { useUserStore } from '../store/UserStore'
+import Navbar from '../components/Navbar'
+import PageHeading from '../components/PageHeading'
 const AdminPanel = () => {
     const usertype = useUserStore((state) => state.usertype)
 
     if (usertype === 'admin') {
         return (
-            <div>AdminPanel</div>
+            <>
+                <Navbar usertype={usertype} />
+                <PageHeading text="Admin Panel"/>
+                <div className='users-stats-container'>
+                    
+                </div>
+            </>
         )
     }
     else {
