6eed1a0fba055f1f174fc42b394e753a265b1c01
ChristopherYammine <chrisyammine98@gmail.com>
2022-07-27 17:13:10 +0300

    frontend : adding responsiveness to the add new category section

--- closer-frontend/src/pages/AdminPanel.js
+++ closer-frontend/src/pages/AdminPanel.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageNotFound from './PageNotFound'
 import { useUserStore } from '../store/UserStore'
 import Navbar from '../components/Navbar'
 import PageHeading from '../components/PageHeading'
 const AdminPanel = () => {
     const usertype = useUserStore((state) => state.usertype)
+    const [catImage, setCatImage] = useState(require('../assets/blankprofile.png'))
 
+    function catImageUploaded(files) {
+        var file = files[0];
+        var reader = new FileReader();
+
+
+        reader.onload = function () {
+            setCatImage(reader.result);
+
+        }
+
+        reader.readAsDataURL(file);
+    }
     if (usertype === 'admin') {
         return (
             <>
@@ -34,7 +47,23 @@ const AdminPanel = () => {
                         </div>
                     </div>
                     <div className='new-cat-container'>
-                        <h1>something</h1>
+                        <div className='cat-container-left'>
+                            <h1>chros</h1>
+                            <label className='profile-photo-desc' htmlFor="fileId2">click here to upload profile picture</label>
+                        </div>
+                        <div className='upload-category'>
+                            <input type="file" name="photo" id="fileId2"
+                                onChange={(e) => { catImageUploaded(e.target.files) }} />
+                            <div className="upload">
+                                <div className='uploaded-photo'>
+                                    <img src={catImage} alt='' />
+                                </div>
+                            </div>
+
+
+
+                        </div>
+
                     </div>
                 </div>
             </>
