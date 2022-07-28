2b828d341dd52a479907150d314d434e12c0f6ec
ChristopherYammine <chrisyammine98@gmail.com>
2022-07-28 05:03:11 +0300

    frontend : styling the expired events table

--- closer-frontend/src/pages/AdminPanel.js
+++ closer-frontend/src/pages/AdminPanel.js
@@ -136,6 +136,34 @@ const AdminPanel = () => {
 
                     </div>
                 </div>
+                <div className='expired-table'>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Age</th>
+                                <th>Gender</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Anom</td>
+                                <td>19</td>
+                                <td>Male</td>
+                            </tr>
+                            <tr>
+                                <td>Megha</td>
+                                <td>19</td>
+                                <td>Female</td>
+                            </tr>
+                            <tr>
+                                <td>Subham</td>
+                                <td>25</td>
+                                <td>Male</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
                 <ToastContainer />
             </>
         )
