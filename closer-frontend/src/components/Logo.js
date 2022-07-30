987465ac6d599799fb3985afab896bf09439e3eb
ChristopherYammine <chrisyammine98@gmail.com>
2022-07-30 18:16:45 +0300

    frontend : cleaning the logo component

--- closer-frontend/src/components/Logo.js
+++ closer-frontend/src/components/Logo.js
@@ -10,8 +10,7 @@ const Logo = () => {
     return (
         <motion.div animate={{
             scale: [1, 2, 2, 1, 1],
-            rotate: [0, 0, 270, 270, 0],
-            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+            rotate: [0, 0, 270, 270, 0]
         }}
             className='logo' >
             <img src={require("../assets/Group.png")} width={100} alt="" onClick={goHome} />
