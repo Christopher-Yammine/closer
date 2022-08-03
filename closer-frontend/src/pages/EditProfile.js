8384f7d9940783ae807795e58b1bdfd71ad20571
ChristopherYammine <chrisyammine98@gmail.com>
2022-08-04 00:16:08 +0300

    frontend : fixing the image section height

--- closer-frontend/src/pages/EditProfile.js
+++ closer-frontend/src/pages/EditProfile.js
@@ -17,41 +17,43 @@ const EditProfile = () => {
         <>
             <Navbar usertype={usertype} />
             <PageHeading text="Edit Profile" />
-            <div className='edit-profile-form-container'>
-                <div className='left-signup'>
-                    <div className='signup-heading'>
-                        Edit your <span className='purple'>Closer</span> account
-                    </div>
-                    <div className='signup-input'>
-                        <input type="text" value={firstName} placeholder='Enter your first name' onChange=
-                            {
-                                (e) => setFirstName(e.currentTarget.value)
-                            }>
+            <div className='edit-profile-main-container'>
+                <div className='edit-profile-form-container'>
+                    <div className='left-signup'>
+                        <div className='signup-heading'>
+                            Edit your <span className='purple'>Closer</span> account
+                        </div>
+                        <div className='signup-input'>
+                            <input type="text" value={firstName} placeholder='Enter your first name' onChange=
+                                {
+                                    (e) => setFirstName(e.currentTarget.value)
+                                }>
 
-                        </input>
-                        <input type="text" placeholder='Enter your last name' value={lastName} onChange={(e) => setLastName(e.currentTarget.value)}></input>
-                        <button type='button' className='signup-button' onClick={''}>Save</button>
-                    </div>
-
-                </div>
-                <div className='right-edit'>
-                    <div className='upload-profile'>
-                        <input type="file" name="photo" id="fileId2"
-                            onChange={(e) => { '' }} />
-                        <div className="upload">
-                            <div className='uploaded-photo-edit'>
-                                <img src={profilePic} alt='' />
-                            </div>
+                            </input>
+                            <input type="text" placeholder='Enter your last name' value={lastName} onChange={(e) => setLastName(e.currentTarget.value)}></input>
+                            <button type='button' className='signup-button' onClick={''}>Save</button>
                         </div>
 
-                        <label className='profile-photo-desc' htmlFor="fileId2">click here to upload profile picture</label>
+                    </div>
+                    <div className='right-edit'>
+                        <div className='upload-profile'>
+                            <input type="file" name="photo" id="fileId2"
+                                onChange={(e) => { '' }} />
+                            <div className="upload-edit">
+                                <div className='uploaded-photo-edit'>
+                                    <img src={profilePic} alt='' />
+                                </div>
+                            </div>
+
+                            <label className='profile-photo-desc' htmlFor="fileId2">click here to upload profile picture</label>
 
 
 
 
+
+                        </div>
 
                     </div>
-
                 </div>
             </div>
         </>
