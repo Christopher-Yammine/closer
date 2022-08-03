e320fd9d954ca687c53ee177a36e08d3a4120fea
ChristopherYammine <chrisyammine98@gmail.com>
2022-08-03 23:40:46 +0300

    frontend : adding the edit profile page

---
+++ closer-frontend/src/pages/EditProfile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import PageHeading from '../components/PageHeading'
+
+const EditProfile = () => {
+    return (
+        <>
+            <Navbar />
+            <PageHeading text="Edit Profile" />
+        </>
+    )
+}
+
+export default EditProfile
